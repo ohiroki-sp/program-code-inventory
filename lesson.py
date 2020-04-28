ce2ce5cf572f498a5d3a3a93a3db69b35d159ff5
ohiroki-sp <64342567+ohiroki-sp@users.noreply.github.com>
2020-04-29 08:59:48 +0900

    list copy
    リストのコピーの仕様

--- lesson.py
+++ lesson.py
@@ -1,22 +1,29 @@
-r = [1, 2, 3,4, 5, 1, 2, 3]
-print(r.index(3,3))
+i = [1, 2, 3, 4, 5]
+j = i
+j[0] = 100
+print('j =',j)
+print('i =',i)
 
-print(r.index(3))
-if 100 in r:
-    print('exist')
 
-r.sort()
-print(r)
-r.sort(reverse=True)
-print(r)
-r.reverse()
-print(r)
-s = 'My name is Mike.'
-to_split = s.split(' ')
-print(to_split)
-x = ' '.join(to_split)
-x = ' ########'.join(to_split)
+x = [1, 2, 3, 4, 5]
+y = x.copy()
+#y = x[:]
+y[0] = 100
+print('y =',y)
+print('x =', x)
 
+x = 20
+y = x
+y = 5
+print(id(x))
+print(id(y))
 print(x)
+print(y)
 
-print(help(list))
+x = ['a','b']
+y = x
+y[0] = 'p'
+print(id(x))
+print(id(y))
+print(x)
+print(y)
